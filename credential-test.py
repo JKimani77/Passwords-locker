69067f1f7e76be0450d918080176cfbb8ebbf0c8
Hewlett.Prd <jkkimani77@gmail.com>
2020-01-27 08:28:48 +0300

    Created a test case for finding credentials by username

--- credential-test.py
+++ credential-test.py
@@ -47,3 +47,16 @@ class TestCredentials(unittest.TestCase):
         test_credentials.save_credentials()
         self.new_credentials.delete_credentials()  # to delete a credentials   object
         self.assertEqual(len(Credentials.credentials_list), 1)
+
+    def test_find_credentials_by_name(self):
+        '''
+        test to check if credentials by the account name and can display information
+        '''   
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Google", "3344")
+        test_credentials.save_credentials()
+        # found_credentials = Credentials.find_by_number("0711491808")
+        # self.assertEqual(found_credentials.credentials_name,
+        #                  test_credentials.password)
+
