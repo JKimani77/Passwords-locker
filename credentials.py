d1b02f78dec25515ce5c44844a44a300b5db84c3
Hewlett.Prd <jkkimani77@gmail.com>
2020-01-27 08:27:07 +0300

    Created a class that instantiates new credentials and defined functions that save and deletes credentials

---
+++ credentials.py
@@ -0,0 +1,25 @@
+class Credentials:
+
+    '''
+    class that generates new instance credentials
+    '''
+
+    def __init__(self, account_name, account_password):
+        self.account_name = account_name
+        self.account_password = account_password
+
+    credentials_list = []
+
+    def save_credentials(self):
+        '''
+        method that saves credentials objects into credentials list
+        '''
+
+        self.credentials_list.append(self)
+
+    def delete_credentials(self):
+        '''
+        method that deletes a credential
+        '''
+
+        Credentials.credentials_list.remove(self)
