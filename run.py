d7db627ac3e736d9133daa795223755ee53d17d1
Hewlett.Prd <jkkimani77@gmail.com>
2020-01-27 10:00:50 +0300

    added imports and defined functions to add, save and delete credentials

---
+++ run.py
@@ -0,0 +1,45 @@
+import random
+from user import User
+from credentials import Credentials
+
+# functions to add credentials
+
+
+def create_new_credential(account_name, account_password):
+    '''
+    method that creates  new  account name
+    '''
+    new_credential = Credentials(account_name, account_password)
+
+    return new_credential
+
+
+def save_new_credential(credentials):
+    '''
+    method to save new credentials
+    '''
+
+    credentials.save_credentials()
+
+
+def find_credential(account_name):
+    '''
+    method to find a credential that has been created
+    '''
+
+    return Credentials.find_by_name(account_name)
+
+
+def display_credential():
+    '''
+    method to dispay a credential that has been created
+    '''
+    return Credentials.display_credentials()
+
+
+def delete_credential(credentials):
+    '''
+    method to delete a credential that has been created
+    '''
+
+    return Credentials.delete_credentials(credentials)
